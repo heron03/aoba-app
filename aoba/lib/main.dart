32eba9ece498a026af05bfb86e39be1b28c845e6
massau <joyce.santos202@etec.sp.gov.br>
2021-04-19 19:10:23 -0300

    Cria estrutura inicial do app

---
+++ aoba/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:aoba/view/produto_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Inicio());
+}
+
+class Inicio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFF304FFE),
+      ),
+      home: ProdutoView(),
+    );
+  }
+}
